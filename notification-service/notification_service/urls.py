4a71e774f1a7f711d843cb6cb2a361003e71f4c9
Kevin Alexander Mejía Mera <km830820@gmail.com>
2026-01-15 22:30:26 -0500

    eliminacion de microservicios

--- notification-service/notification_service/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/email/', include('apps.email.urls')),
-    path('api/sms/', include('apps.sms.urls')),
-    path('api/push/', include('apps.push.urls')),
-]
--- restaurant-service/apps/menu/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('health/', views.health_check, name='health'),
-    path('test-auth/', views.test_auth_view, name='test-auth'),
-]
